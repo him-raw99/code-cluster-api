4d9e7ece49e6aad97fb3eed9a155a58674fc68b4
Himanshu Rawat <97152850+him-raw99@users.noreply.github.com>
2023-03-13 17:56:17 +0530

    fix profile bug

--- routes/profileRoute.js
+++ routes/profileRoute.js
@@ -3,6 +3,6 @@ import { getProfile } from "../controllers/profileUtils.js";
 import verify from "../middleware/authMiddleware.js";
 const router = Router();
 
-router.get("/profile", verify, getProfile);
+router.get("/", verify, getProfile);
 
 export default router;
